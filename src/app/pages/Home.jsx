e630a57e50912f97293659cbb5292ae8190b1316
Víctor <victor.martinez.dk@gmail.com>
2025-12-29 18:35:13 +0100

    Landing page final

--- src/app/pages/Home.jsx
+++ src/app/pages/Home.jsx
@@ -3,6 +3,18 @@ import { useNavigate } from "react-router-dom";
 import "../../styles/homeView.css";
 
 export const Home = () => {
+
+ const navigate = useNavigate();
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            navigate("/catalog");
+        }, 5000); // 3 segundos
+
+        // Limpieza del timeout
+        return () => clearTimeout(timer);
+    }, [navigate]);
+    
   return (
 
     <div className='containerHome' id="home">
