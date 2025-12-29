f3830b7de582308d9e19c05c11fd5e23636fc7d1
Víctor <victor.martinez.dk@gmail.com>
2025-12-29 18:30:14 +0100

    Landing page

--- src/app/pages/Home.jsx
+++ src/app/pages/Home.jsx
@@ -1,9 +1,20 @@
+<<<<<<< Updated upstream
 
+=======
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "../../styles/homeView.css";
+>>>>>>> Stashed changes
 
 export const Home = () => {
   return (
-    <div className='container home' id="home">
-      Home 
+    <div className='containerHome' id="home">
+      <div className='containerHome__intro'>
+        <h1>Bienvenido a la librería "Relatos de papel"</h1>
+        <h3>Redirigiendo en 5 segundos</h3>
+        <div className="containerHome__loader"></div>
+      </div>
+      <button className="containerHome__button--catalog" onClick={ () => navigate(`/catalog}`) }>Acceder al catálogo</button>
     </div>
   )
 }
