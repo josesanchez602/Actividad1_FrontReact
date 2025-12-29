1973d7415d568e72bf1f5bce9d83204f0a89beba
clara <e.cdelgom@ms.ugr.es>
2025-12-29 19:30:28 +0100

    con metodología Bem

--- src/app/components/BookResult.jsx
+++ src/app/components/BookResult.jsx
@@ -1,13 +1,32 @@
+// import { BookGrid } from './BookGrid.jsx';
+
+// export function BookResults({ books, searchTerm }) {
+//   if (books.length === 0) {
+//     return (
+//       <p className="text-gray-500 text-center">
+//         No se encontraron libros con "{searchTerm}"
+//       </p>
+//     );
+//   }
+
+//   return <BookGrid books={books} />;
+// }
+
 import { BookGrid } from './BookGrid.jsx';
+import '../../styles/book-results.css';
 
 export function BookResults({ books, searchTerm }) {
   if (books.length === 0) {
     return (
-      <p className="text-gray-500 text-center">
+      <p className="book-results__empty">
         No se encontraron libros con "{searchTerm}"
       </p>
     );
   }
 
-  return <BookGrid books={books} />;
+  return (
+    <div className="book-results">
+      <BookGrid books={books} />
+    </div>
+  );
 }
--- src/app/components/SearchBar.jsx
+++ src/app/components/SearchBar.jsx
@@ -1,12 +1,28 @@
+// export function SearchBar({ searchTerm, setSearchTerm }) {
+//   return (
+//     <div className="mb-6">
+//       <input
+//         type="text"
+//         placeholder="Buscar por título"
+//         value={searchTerm}
+//         onChange={(e) => setSearchTerm(e.target.value)}
+//         className="w-full px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+//       />
+//     </div>
+//   );
+// }
+//import './search-bar.css';
+//import "../styles/cartButton.css";
+import "../../styles/search-bar.css"
 export function SearchBar({ searchTerm, setSearchTerm }) {
   return (
-    <div className="mb-6">
+    <div className="search-bar">
       <input
         type="text"
         placeholder="Buscar por título"
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
-        className="w-full px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        className="search-bar__input"
       />
     </div>
   );
