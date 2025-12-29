4aee20e405ef7c5c2f5fd33b12cb068ff5ad8a75
Víctor <victor.martinez.dk@gmail.com>
2025-12-29 17:45:56 +0100

    temporizador de redirección página principal a catálogo

--- src/app/pages/Home.jsx
+++ src/app/pages/Home.jsx
@@ -1,6 +1,20 @@
-
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const Home = () => {
+
+ const navigate = useNavigate();
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            navigate("/catalog");
+        }, 5000); // 3 segundos
+
+        // Limpieza del timeout
+        return () => clearTimeout(timer);
+    }, [navigate]);
+
+
   return (
     <div className='container home' id="home">
       Home 
